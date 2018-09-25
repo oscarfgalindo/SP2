4302705473ea33f381df19c862eada58f421ce79
Janie <janiel.neal@gmail.copm>
2018-09-25 13:50:05 -0700

    Added To-Do's and moved main to new file

---
+++ src/sp2.java
@@ -0,0 +1,66 @@
+import java.awt.Dimension;
+import java.awt.Point;
+import java.io.IOException;
+
+
+import edu.uci.ics.jung.algorithms.layout.Layout;
+import edu.uci.ics.jung.algorithms.layout.StaticLayout;
+import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.graph.SparseGraph;
+import edu.uci.ics.jung.graph.util.Pair;
+
+
+public class sp2 {
+
+	public static void main(String[] args) throws IOException {
+		Graph<String, String> g = new SparseGraph<String, String>();
+
+		g.addVertex("000");
+		g.addVertex("001");
+		g.addVertex("010");
+		g.addVertex("100");
+		g.addVertex("011");
+		g.addVertex("101");
+		g.addVertex("110");
+		g.addVertex("111");
+		g.addEdge("e0", new Pair<String>("000", "001"));
+		g.addEdge("e1", new Pair<String>("000", "010"));
+		g.addEdge("e2", new Pair<String>("000", "100"));
+		g.addEdge("e3", new Pair<String>("001", "011"));
+		g.addEdge("e4", new Pair<String>("001", "101"));
+		g.addEdge("e5", new Pair<String>("010", "011"));
+		g.addEdge("e6", new Pair<String>("010", "110"));
+		g.addEdge("e7", new Pair<String>("100", "101"));
+		g.addEdge("e8", new Pair<String>("100", "110"));
+		g.addEdge("e9", new Pair<String>("011", "111"));
+		g.addEdge("e10", new Pair<String>("101", "111"));
+		g.addEdge("e11", new Pair<String>("110", "111"));
+
+		// TODO: Compute a force directed layout
+
+		Dimension dim = new Dimension(400, 400);
+		// (0,0) is the top left corner
+		Layout<String, String> l = new StaticLayout<String, String>(g);
+		l.setLocation("000", new Point(100, 50));
+		l.setLocation("001", new Point(300, 50));
+		l.setLocation("010", new Point(100, 350));
+		l.setLocation("100", new Point(50, 100));
+		l.setLocation("011", new Point(300, 350));
+		l.setLocation("101", new Point(350, 100));
+		l.setLocation("110", new Point(50, 300));
+		l.setLocation("111", new Point(350, 300));
+		
+		/*VisualizationImageServer<String, String> vis = new VisualizationImageServer<String, String>(l, dim);
+
+		BufferedImage im = (BufferedImage) vis.getImage(new Point2D.Double(dim.getWidth() / 2, dim.getHeight() / 2),
+				dim);
+		ImageIO.write((RenderedImage) im, "jpg", new File("out.jpg"));
+		for (String e : g.getEdges()) {
+			System.out.println(e);
+		}*/
+		
+		sp2starter tools = new sp2starter();
+		tools.fRAlgorithm(g, 1, dim);
+	}
+
+}
--- src/sp2starter.java
+++ src/sp2starter.java
@@ -22,65 +22,26 @@ import edu.uci.ics.jung.graph.event.GraphEvent.Vertex;
 import edu.uci.ics.jung.graph.util.Pair;
 import edu.uci.ics.jung.visualization.VisualizationImageServer;
 
-public class sp2starter {
-	public static void main(String[] args) throws IOException {
-		Graph<String, String> g = new SparseGraph<String, String>();
+public class sp2starter {	
 
-		g.addVertex("000");
-		g.addVertex("001");
-		g.addVertex("010");
-		g.addVertex("100");
-		g.addVertex("011");
-		g.addVertex("101");
-		g.addVertex("110");
-		g.addVertex("111");
-		g.addEdge("e0", new Pair<String>("000", "001"));
-		g.addEdge("e1", new Pair<String>("000", "010"));
-		g.addEdge("e2", new Pair<String>("000", "100"));
-		g.addEdge("e3", new Pair<String>("001", "011"));
-		g.addEdge("e4", new Pair<String>("001", "101"));
-		g.addEdge("e5", new Pair<String>("010", "011"));
-		g.addEdge("e6", new Pair<String>("010", "110"));
-		g.addEdge("e7", new Pair<String>("100", "101"));
-		g.addEdge("e8", new Pair<String>("100", "110"));
-		g.addEdge("e9", new Pair<String>("011", "111"));
-		g.addEdge("e10", new Pair<String>("101", "111"));
-		g.addEdge("e11", new Pair<String>("110", "111"));
+	public void fRAlgorithm(Graph<String, String> g, int k, Dimension dim) {
+		
+		double height = dim.getHeight();
+		double width = dim.getWidth();
+		
+		HashMap<String, Tuple> displacements = new HashMap<String, Tuple>();
+		HashMap<String, Tuple> positions = new HashMap<String, Tuple>();
+		
+		//TODO randomly assign first positions and populate positions HashMap
+		
+		
 
-		// TODO: Compute a force directed layout
-
-		Dimension dim = new Dimension(400, 400);
-		// (0,0) is the top left corner
-		Layout<String, String> l = new StaticLayout<String, String>(g);
-		l.setLocation("000", new Point(100, 50));
-		l.setLocation("001", new Point(300, 50));
-		l.setLocation("010", new Point(100, 350));
-		l.setLocation("100", new Point(50, 100));
-		l.setLocation("011", new Point(300, 350));
-		l.setLocation("101", new Point(350, 100));
-		l.setLocation("110", new Point(50, 300));
-		l.setLocation("111", new Point(350, 300));
-
-		VisualizationImageServer<String, String> vis = new VisualizationImageServer<String, String>(l, dim);
-
-		BufferedImage im = (BufferedImage) vis.getImage(new Point2D.Double(dim.getWidth() / 2, dim.getHeight() / 2),
-				dim);
-		ImageIO.write((RenderedImage) im, "jpg", new File("out.jpg"));
-		for (String e : g.getEdges()) {
-			System.out.println(e);
-		}
-	}
-
-	public void fRAlgorithm(Graph<String, String> g, int k) {
-
-		for (int t = 0; t < 100; t++) {
+		
+		for (int t = 0; t < 1; t++) {
 			Tuple displacementTemp = new Tuple(0, 0); // allowed due to the (int,int) constructor
 			
-			
-			
-			
-			HashMap<String, Tuple> displacements = new HashMap<String, Tuple>();
-			HashMap<String, Tuple> positions = new HashMap<String, Tuple>();
+			double d; //temp for distance
+
 
 			for (String e : g.getEdges()) {
 
@@ -88,25 +49,36 @@ public class sp2starter {
 				
 				String a = edge.getFirst();
 				String b = edge.getSecond();
+				System.out.println(a);
+	
+				// TODO: calculate attractive force			
+//				d = 
+//				displacementTemp = attractF();
 				
-				
-				
-				displacements.get(e).sum(displacementTemp);
-				System.out.println(e);
-				System.out.println(displacements.get(e));
+//				displacements.get(e).sum(displacementTemp);
+//				System.out.println(e);
+//				System.out.println(displacements.get(e));
 
 			}
-			// for(String v: g.getVertices()){
-			// positions.put(v, 0);
-			//
-			// }
-			// }
-			for (String e : g.getEdges()) {
-				System.out.println(e);
-
-			}
-
+			
+			 
+//			for(String u: g.getVertices()){
+//				 for(String v: g.getVertices()){
+//					 if( u != v) {
+//						 //TODO: add repulsive force to u displacement
+//					 }
+//						
+//				 }
+//					
+//			 }
+//		
+//			for(String v: g.getVertices()){				 
+//				//TODO: update v's position
+//			 }
+//		
 		}
+		
+		//TODO: create and return static layout with calculated positions 
 	}
 
 	public double distance(Tuple t, Tuple u) {
